ac47e121d108b7f3e7c45e9fc3aa7d4e3ce6b7fc
Paulius Mačernis <sugalvojau@gmail.com>
2018-04-30 19:42:09 +0300

    We define a `shutdown` function so that we can properly kill the Confd and `php-fpm` background processes running when an interrupt signal is sent (`Ctrl`+`C`). Next, we run the `confd -onetime` command every `5` seconds until it succeeds. The Confd command references `consul:8500`, which is how the application container makes requests to the Consul service on the network. Once Confd succeeds, we run it in the background and poll consul every `10` seconds (`-interval 10`). When the Consul backend is updated, Confd detects the changes and regenerates the `Caddyfile`.
    `start.sh` updates the `Caddyfile` on a value change in Consul.
    
    Using the Consul backend with Confd means that we need to update our template to use the `getv` function instead of `getenv`, which is specific to using environment variables. The other Confd backends use `getv`, so you could change out Consul for another backend, and the template wouldn't change.
    
    We needed to remove the `on startup php-fpm` event because when we send the restart signal to Caddy, it tries to start `php-fpm` again. We avoid this by just starting `php-fpm` in the `start.sh` file.
    
    When we first run the containers, the application cannot find the consul key, so the CMD script runs the until loop until it running confd succeeds.
    
    Let's run the container and manually populate consul to get the container running:
    `docker-compose up --build`
    `curl -X PUT -d 'www.example.com' http://localhost:8500/v1/kv/example.com/hostname`
    (check for: http://localhost:8500/ui/#/dc1/kv/example.com/hostname/edit )
    (check for: http://www.example.com:8080 before updating hosts file to point www.example.com to 127.0.0.1)
    `curl -X PUT -d 'foo.com' http://localhost:8500/v1/kv/example.com/hostname`
    (check for: http://www.example.com:8080 )
    
    Now if you request http://www.example.com:8080 you should get the following response from caddy: "404 Site www.example.com:8080 is not served on this interface". Note, that this only works if you update your hosts file to make www.example.com point to 127.0.0.1.

--- start.sh
+++ start.sh
@@ -2,10 +2,27 @@
 
 set -e
 
-confd -onetime -backend env
+function shutdown {
+    kill -s SIGTERM $CONFD_PID
+    wait $CONFD_PID
+    kill -s SIGTERM $PHP_FPM_PID
+    wait $PHP_FPM_PID
+}
+
+# Wait until the initial configuration succeeds
+until confd -onetime -backend consul -node consul:8500; do
+    echo "Waiting for the initial confd configuration"
+    sleep 5
+done
+
+confd -interval 10 -backend consul -node consul:8500 &
+CONFD_PID=$!
+
+php-fpm &
+PHP_FPM_PID=$!
+
+trap shutdown SIGTERM SIGINT
 
 /usr/bin/caddy -validate --agree=true --conf=/etc/Caddyfile
 
 exec /usr/bin/caddy --agree=true --conf=/etc/Caddyfile
-
-#trap : TERM INT; sleep infinity & wait
